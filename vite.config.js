9be5f5f3eead9e73d10bd8fa6585dfddd8186508
giangnn-git <nngiang252@gmail.com>
2025-11-23 00:05:22 +0700

    Update v4

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "https://orthopterous-unwieldable-kristal.ngrok-free.dev", // tam
-        target: "https://junie-preaortic-sina.ngrok-free.dev", // QA
+        // target: "https://junie-preaortic-sina.ngrok-free.dev", // QA
+        target: "https://swp391su25se1839fe-production.up.railway.app",
 
         changeOrigin: true,
         secure: false,
